9475f4b37d05f2d85fe839a4c2cf5292e97a3370
ZingyMinotaur <41849753+ZingyMinotaur@users.noreply.github.com>
2019-02-21 18:34:06 -0600

    Update Main.java

--- src/assignment/Main.java
+++ src/assignment/Main.java
@@ -9,13 +9,10 @@ public class Main {
 
 class NamePrinter {
     /**
-<<<<<<< HEAD
      * This method prints the concatenation of the elements with the delimiter. 
      * For this case, the elements is the names of the team members and the delimiter is a dash.
      * The output will have the names of all team members separated by a dash.
-=======
      * Prints the names of the group members separated by commas.
->>>>>>> 7e8103d5cf2d365a35278e49f885b7c305656252
      */
     public void printNames() {
         String separator = ",";
