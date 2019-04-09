f86f155612966bd184d24ef383c8f42fbff61cb7
ZingyMinotaur <41849753+ZingyMinotaur@users.noreply.github.com>
2019-04-09 18:30:44 -0500

    Update ShoppingCartTest.java

--- A4 - Junit assignment/ShoppingCartTest.java
+++ A4 - Junit assignment/ShoppingCartTest.java
@@ -5,13 +5,15 @@ import static org.junit.Assert.*;
 public class ShoppingCartTest {
     private ShoppingCart testCart = new ShoppingCart();
     private Product soda = new Product("Coca Cola", 2.50);
-
+   
+    //Test 1
     //Tests to see if the initial cart has 0 items
     @Test
     public void testInitialCart() throws Exception {
         assertEquals(0, testCart.getItemCount());
     }
 
+    //Test 2
     //Tests to see if the empty method empties the shopping cart properly.
     //This is done by adding an item to the cart, invoke the empty method, then testing.
     @Test
@@ -21,6 +23,14 @@ public class ShoppingCartTest {
         assertEquals(0, testCart.getItemCount());
     }
 
-
+    //Test 4
+    //Tests to see if the balance is totaled correctly.
+    //This is done by adding an item to the cart (which is known to be of 0 balance)
+    //and checking the balance against the price of the item.
+    @Test
+    public void testBalance() throws Exception {
+        testCart.addItem(soda);
+        assertEquals(2.5, testCart.getBalance(), 0);
+    }
 
 }
