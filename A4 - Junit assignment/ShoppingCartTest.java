b9b5cad0c937222a88e45d245f96dcf0d4679645
ZingyMinotaur <41849753+ZingyMinotaur@users.noreply.github.com>
2019-04-09 18:17:25 -0500

    Create ShoppingCartTest.java

---
+++ A4 - Junit assignment/ShoppingCartTest.java
@@ -0,0 +1,26 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ShoppingCartTest {
+    private ShoppingCart testCart = new ShoppingCart();
+    private Product soda = new Product("Coca Cola", 2.50);
+
+    //Tests to see if the initial cart has 0 items
+    @Test
+    public void testInitialCart() throws Exception {
+        assertEquals(0, testCart.getItemCount());
+    }
+
+    //Tests to see if the empty method empties the shopping cart properly.
+    //This is done by adding an item to the cart, using the empty method, then testing.
+    @Test
+    public void testEmptyCart() throws Exception {
+        testCart.addItem(soda);
+        testCart.empty();
+        assertEquals(0, testCart.getItemCount());
+    }
+
+
+
+}
