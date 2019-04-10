a790ae1f8763acc096bdec6376b9c46ed6505c97
srm150530 <srm150530@utdallas.edu>
2019-04-09 22:52:59 -0500

    Update player.java
    added a few basics to the player class

--- Crazy 8s implementation/src/player.java
+++ Crazy 8s implementation/src/player.java
@@ -1,2 +1,23 @@
+import java.util.*;
+
 public class player {
+    private String playerName;
+    private int playerNumber;
+    private int score;
+    private List<card> cardsOnHand = new ArrayList<card>();
+    
+    public player(String name, int playerNum){
+        playerName = name;
+        playerNumber = playerNum;
+        score = 0;
+    }
+    
+    public int getScore(){
+        return score;
+    }
+    
+    public void setScore(int points){
+        score = points;
+    }
+    
 }
