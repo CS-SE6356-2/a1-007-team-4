b580f518bddf0a6b095f5b83acf17009fe6a8680
srm150530 <srm150530@utdallas.edu>
2019-04-18 12:18:27 -0500

    Update CardGame.java
    added some of the logic for when the game is created

--- Crazy 8s implementation/src/CardGame.java
+++ Crazy 8s implementation/src/CardGame.java
@@ -1,14 +1,27 @@
+import java.util.*;
 
+public class CardGame
+{
+    private Deck deck;
+    private ArrayList<Card> discardPile;
+    private ArrayList<Player> players;
+    
 
-public class cardGame {
-
-	// Deck used
-	deck newDeck = new deck();
-
-
-	// Main entry
-    public static void main(String[] args){
-
+    public CardGame(ArrayList<Player> players)
+    {
+        this.players = players;
+        deck = new Deck();
+        deck.shuffle();
+        
+        for (int i=0; i < 5; i++){
+            for(int j=0; j < players.size(); j++){
+                players.get(j).addCard(deck.drawTopCard());
+            }
+        }
+        
+        discardPile.add(0, deck.drawTopCard());
     }
 
+    
+   
 }
