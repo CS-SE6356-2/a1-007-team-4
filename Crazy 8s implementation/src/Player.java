5fb2dead29bca680fb7d371643b9e599a382dde7
srm150530 <srm150530@utdallas.edu>
2019-04-18 12:08:46 -0500

    Update Player.java
    Added some new methods to the player class.

--- Crazy 8s implementation/src/Player.java
+++ Crazy 8s implementation/src/Player.java
@@ -1,23 +1,53 @@
 import java.util.*;
 
-public class player {
-    private String playerName;
-    private int playerNumber;
+public class Player
+{
+    private String name;
     private int score;
-    private List<card> cardsOnHand = new ArrayList<card>();
-    
-    public player(String name, int playerNum){
-        playerName = name;
-        playerNumber = playerNum;
-        score = 0;
+    private ArrayList<Card> hand; 
+
+    public Player(String name){
+        this.name = name;
+        hand = new ArrayList<Card>();
+    }
+
+    public String getName(){
+        return name;
     }
     
-    public int getScore(){
+    public void setName(String name){
+        this.name = name;
+    }
+    
+    private int getScore(){
         return score;
     }
     
-    public void setScore(int points){
-        score = points;
+    private void addPoints(int points){
+        score += points;
     }
     
+    public ArrayList<Card> getHand(){
+        return hand;
+    }
+    
+    public Card getCard(int index) {
+        return hand.get(index);
+    }
+    
+    public void addCard(Card card) {
+        hand.add(card);
+    }
+    
+    public Card removeCard(int index) {
+        return hand.remove(index);
+    }
+    
+    public int size(){
+        return hand.size();
+    }
+    
+    public boolean empty() {
+        return hand.size() == 0;
+    }
 }
