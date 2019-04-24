be3f1b9221160b9ac31b1af500cde47ddfbb86cd
Andy Hudson <andyhudson725@gmail.com>
2019-04-24 16:34:21 -0500

    Improved menu appearance

--- src/gui/menu/MainMenu.java
+++ src/gui/menu/MainMenu.java
@@ -1,7 +1,9 @@
 package gui.menu;
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -15,6 +17,8 @@ import gui.menu.players.PlayerAdder;
 public class MainMenu extends JComponent {
 	// JComponent serial ID
 	private static final long serialVersionUID = 1L;
+	// Background color
+	private static final Color bgcol = new Color(255, 200, 127);
 
 	// Parent container
 	private final GameStateManager parent;
@@ -33,20 +37,31 @@ public class MainMenu extends JComponent {
 		setLayout(null);
 
 		// Initialize player adder at position
-		playerAdder = new PlayerAdder(wid / 2, hei / 3, wid / 3, hei / 4);
+		playerAdder = new PlayerAdder(wid / 4, hei / 3, wid / 2, hei / 4);
 		// Add player adder
 		add(playerAdder);
 
 		// Initialie title
 		JLabel title = new JLabel("Crazy 8's");
-		title.setFont(new Font("TimesRoman", Font.BOLD, 24));
-		title.setLocation(wid / 2, hei / 4);
+		title.setFont(new Font("TimesRoman", Font.BOLD, 36));
+		title.setHorizontalAlignment(JLabel.CENTER);
+		title.setBounds(wid / 3, hei / 12, wid / 3, hei / 6);
+		title.setForeground(new Color(127, 0, 0));
 		// Add title
 		add(title);
+		// Initialize title background
+		JLabel titlebg = new JLabel("Crazy 8's");
+		titlebg.setFont(new Font("TimesRoman", Font.BOLD, 36));
+		titlebg.setHorizontalAlignment(JLabel.CENTER);
+		final int shift = 2;
+		titlebg.setBounds(wid / 3 + shift, hei / 12 + shift, wid / 3, hei / 6);
+		titlebg.setForeground(new Color(63, 63, 63));
+		// Add title background
+		add(titlebg);
 
 		// Initialize start button
 		JButton start = new JButton("Start Game");
-		start.setBounds(wid / 3, hei * 2 / 3, wid / 3, hei / 6);
+		start.setBounds(wid / 3, hei * 2 / 3, wid / 3, hei / 12);
 		// Configure to call pressStart on click
 		start.addActionListener(new ActionListener() {
 			@Override
@@ -67,5 +82,15 @@ public class MainMenu extends JComponent {
 			// Start with player names
 			parent.startGame(playerAdder.getPlayers());
 		}
+	}
+
+	// Fill background behind rendering
+	@Override
+	public void paint(Graphics g) {
+		// Fill background
+		g.setColor(bgcol);
+		g.fillRect(getX(), getY(), getWidth(), getHeight());
+		// Do regular
+		super.paint(g);
 	}
 }
